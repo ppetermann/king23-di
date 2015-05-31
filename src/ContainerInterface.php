a01252a48ae444a2eb6bea95b1799c54d39808ad
Peter Petermann <peter.petermann@gameforge.com>
2015-05-31 15:53:43 +0200

    complete comments

--- src/ContainerInterface.php
+++ src/ContainerInterface.php
@@ -7,6 +7,7 @@ interface ContainerInterface
      * register an service implementation as a singleton (shared instance)
      * @param $interface
      * @param callable $implementation
+     * @return void
      * @throws \Exception
      */
     public function register($interface, callable $implementation);
@@ -16,6 +17,7 @@ interface ContainerInterface
      *
      * @param $interface
      * @param callable $implementation
+     * @return void
      * @throws \Exception
      */
     public function registerFactory($interface, callable $implementation);
